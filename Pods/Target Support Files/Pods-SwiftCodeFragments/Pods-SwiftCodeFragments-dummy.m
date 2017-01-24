6a06d1969006941afa1d27d255ff52268be0fb31
张明炜 <mingwei.zhang@speedx.com>
2017-01-24 16:18:47 +0800

    升级本地ruby到2.3.3,成功生成了.xcworkspace文件

---
+++ Pods/Target Support Files/Pods-SwiftCodeFragments/Pods-SwiftCodeFragments-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftCodeFragments : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftCodeFragments
+@end
---
+++ Pods/Target Support Files/Pods-SwiftCodeFragments/Pods-SwiftCodeFragments-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SwiftCodeFragmentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftCodeFragmentsVersionString[];
+
